1bc510618697c278ebd0ce13c14a07bbbae0044e
Seppi <josefnpat@gmail.com>
2020-05-20 01:53:48 -0500

    Adding debug keys to test scaling and alignment.

--- main.lua
+++ main.lua
@@ -2,9 +2,12 @@ utf8 = require"utf8"
 
 reflowprint = require"init"
 
-function badprint(progress,text,x,y,w,a)
+scalex,scaley = 1,1
+alignment = "center"
+
+function badprint(progress,text,x,y,w,a,sx,sy)
 	local t = text:sub(1, #text*progress)
-	love.graphics.printf(t,x,y,w,a)
+	love.graphics.printf(t,x,y,w,a,0,sx,sy)
 end
 
 local letters_per_second = 25
@@ -23,7 +26,7 @@ end
 
 function love.draw()
 	dt = dt + love.timer.getDelta()
-	love.window.setTitle(text)
+	love.window.setTitle("ScaleX: "..scalex.." ScaleY: "..scaley.." Alignment: "..alignment.." Text: "..text)
 
 	local rpx,rpy,rpw,rph = 32,32,love.graphics.getWidth()/2-64,9000
 	local bpx,bpy,bpw,bph = love.graphics.getWidth()/2+32,32,love.graphics.getWidth()/2-64,9000
@@ -34,10 +37,10 @@ function love.draw()
 	love.graphics.setFont(newFont)
 
 	love.graphics.rectangle("line",rpx,rpy,rpw,rph)
-	reflowprint(dt/t,text,rpx,rpy,rpw,"center")
+	reflowprint(dt/t,text,rpx,rpy,rpw,alignment,scalex,scaley)
 
 	love.graphics.rectangle("line",bpx,bpy,bpw,bph)
-	badprint(dt/t,text,bpx,bpy,bpw,"center")
+	badprint(dt/t,text,bpx,bpy,bpw,alignment,scalex,scaley)
 
 	love.graphics.setFont(defaultFont)
 	love.graphics.line(0,0,love.graphics.getWidth()*dt/t,0)
@@ -53,6 +56,23 @@ function love.keypressed(key)
 		local byteoffset = utf8.offset(text, -1)
 		if byteoffset then
 			text = string.sub(text, 1, byteoffset - 1)
+		end
+	end
+	if key == "left" then
+		scalex = scalex/2
+	elseif key == "right" then
+		scalex = scalex*2
+	elseif key == "up" then
+		scaley = scaley/2
+	elseif key == "down" then
+		scaley = scaley*2
+	elseif key == "space" then
+		if alignment == "left" then
+			alignment = "center"
+		elseif alignment == "center" then
+			alignment = "right"
+		else--if alignment == "right" then
+			alignment = "left"
 		end
 	end
 end
